bf592a1de28fd75fbeb546009709bd55c4ac9d87
fslong <fsl470657570@outlook.com>
2018-09-20 11:30:09 +0800

    修改百度ip接口

--- webapps/APPs/address.py
+++ webapps/APPs/address.py
@@ -13,8 +13,7 @@ import requests
 
 
 def getAdressByIP(ip='112.103.201.146'):
-    url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query='+ip + \
-        '&co=&resource_id=6006&t=1529930205766&ie=utf8&oe=gbk&cb=op_aladdin_callback&format=json&tn=baidu&cb=jQuery110208006588187590413_1529929325310&_=1529929325336'
+    url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query='+ip+'&resource_id=6006&format=json'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17692'}
     data = {}
